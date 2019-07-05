8a611df53a2e074268bea45ba6b27cbbdf6c830f
Alexander Augustin <alexander@Alexanders-MacBook-Pro.local>
2019-07-04 17:37:20 -0700

    Added the links to the navbar links

--- client/src/components/sections/navbar/navLinks/NavLinks.js
+++ client/src/components/sections/navbar/navLinks/NavLinks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavHashLink as NavLink } from 'react-router-hash-link';
 
 import Button from '../../../common/button/Button'
 
@@ -7,12 +8,26 @@ import styles from './NavLinks.module.css';
 const navSections = () => {
     return (
         <ul id={styles.navSections}> 
-            <li>About</li>
-            <li>Projects</li>
-            <li>Career</li>
-            <li>Contact</li>
+            <li>
+                <NavLink 
+                    smooth to="../App/App#about" 
+                    activeClassName="selected"
+                    scroll={el => el.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"})}
+                >
+                    About
+                </NavLink>
+            </li>
+
+
+
+            <li><NavLink smooth to="../App/App#projects" activeClassName="selected">Projects</NavLink></li>
+            <li><NavLink smooth to="../App/App#career" activeClassName="selected">Career</NavLink></li>
+            <li><NavLink smooth to="../App/App#contact" activeClassName="selected">Contact</NavLink></li>
+            
             <span id={styles.resumeButton}>
-                <Button>Resume</Button>
+                <a href="https://www.dropbox.com/s/t7bmk5f2jplqc9k/alexander_augustin_resume.pdf?dl=0" target="_blank">
+                    <Button>Resume</Button>
+                </a>
             </span>
         </ul>
     )
