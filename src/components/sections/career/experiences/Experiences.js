550851f723e625e6d0887bc740b34dafbd68beac
Alexander Augustin <alexander@Alexanders-MacBook-Pro-2.local>
2019-09-04 18:43:53 -0400

    Updated experience description

--- src/components/sections/career/experiences/Experiences.js
+++ src/components/sections/career/experiences/Experiences.js
@@ -19,6 +19,12 @@ class Experiences extends Component {
         active: 'visa'
     }
 
+    setResearch = () => {
+        this.setState({
+            active: 'research'
+        })
+    }
+
     setVisaActive = () => {
         this.setState({
             active: 'visa'
@@ -37,29 +43,39 @@ class Experiences extends Component {
         })
     }
 
-    setITActive = () => {
-        this.setState({
-            active: 'it'
-        })
-    }
-
     render() {
 
         var experience = null;
 
         switch(this.state.active) {
+            case ('research'):
+                experience = (
+                    <Experience
+                    company = "University of Florida"
+                    position = "Undergraduate Researcher"
+                    date = "August 2019 - Present"
+                    location="Gainesville, Florida"
+                    >
+                    <ul>
+                        <li>Researching, designing and developing a user interface to facilitate the interaction between humans and the NAO robot </li>
+                        <li>Extending functionalities of existing software to further demo the tool to introduce students to robotics</li>
+                    </ul>
+                    </Experience>
+                )
+                break;
+            
             case ('visa'):
                 experience = (
                     <Experience
-                    logo = {exxonMobilIcon}
                     company = "Visa"
                     position = "Software Engineering Intern"
                     date = "June 2019 - August 2019"
                     location="Foster City, California"
                     >
                     <ul>
-                        <li>Developed a GraphQL backend service that returns the exacted data required to the client</li>
-                        <li>Reduced the number of HTTP calls to APIs by implementing GraphQL queries on the front-end using Apollo, React, Redux and Redux-Saga to fetch data asynchronously</li>
+                        <li>Developed a GraphQL backend service that returns the exacted data requested by the client</li>
+                        <li>Reduced the number of HTTP calls to APIs with GraphQL queries on the client-side using Apollo</li>
+                        <li>Implemented Redux for better state management and Redux-Saga for better application side-effects management</li>
                     </ul>
                     </Experience>
                 )
@@ -68,16 +84,15 @@ class Experiences extends Component {
             case ('exxon'):
                 experience = (
                     <Experience
-                    logo = {exxonMobilIcon}
                     company = "Exxon Mobile"
                     position = "UI/UX Researcher"
-                    date = "March 2019 - April 2019"
+                    date = "February 2019 - April 2019"
                     location="Gainesville, Florida"
                     >
                     <ul>
-                        <li>Worked on the design process of a web application for the company’s events</li>
-                        <li>Interviewed clients, defined user personas, created user stories and generated ideas for the design</li>
-                        <li>Created wireframes and designed a prototype of the application using Sketch App</li>
+                        <li>Completed the design process of an event web application for the company’s events</li>
+                        <li>Interviewed clients to understand their needs, defined user personas, created user stories and generated ideas for the design</li>
+                        <li>Created effective wireframes and designed a prototype of the application to show case the output of the research</li>
                     </ul>
                     </Experience>
                 )
@@ -86,7 +101,6 @@ class Experiences extends Component {
             case ('sec'):
                 experience = (
                     <Experience
-                    logo = {exxonMobilIcon}
                     company = "Software Engineering Club"
                     position = "Webmaster"
                     date = "September 2018 - December 2019"
@@ -95,24 +109,6 @@ class Experiences extends Component {
                     <ul>
                         <li>Designed and implemented a new website to facilitate the connections with members and sponsors</li>
                         <li>Collaborate with the executive officers to plan activities and determine actions necessary for the growth of the club</li>
-                    </ul>
-                    </Experience>
-                )
-                break;
-
-            case ('it'):
-                experience = (
-                    <Experience
-                    logo = {exxonMobilIcon}
-                    company = "Info Tech"
-                    position = "SHadower"
-                    date = "October 2017 - November 2017"
-                    location="Gainesville, Florida"
-                    >
-                    <ul>
-                        <li>
-                            Learned JavaScript and became familiar with the Git command line tool to work on a map application of their building.
-                        </li>
                     </ul>
                     </Experience>
                 )
@@ -134,6 +130,8 @@ class Experiences extends Component {
                     
                     <div className={styles['experiences-content']}>
                         <div className={styles.tabs}>
+                            <p onClick={this.setResearch}> UF Research </p>
+
                             <p 
                                 className={styles.active__tab}
                                 // className={this.state.active === 'visa' ? 'styles.active__tab' : ''}
@@ -142,7 +140,6 @@ class Experiences extends Component {
 
                             <p onClick={this.setExxonActive}> Exxon Mobile </p>
                             <p onClick={this.setSECActive}> SEC</p>
-                            <p onClick={this.setITActive}> Info Tech</p>
                         </div>
                         <div>
                             {experience}
